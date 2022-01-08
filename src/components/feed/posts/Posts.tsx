2e2d276b64b0f65e2f39df3354d3a58bdb14421b
Adil Rasiyani <adil.rasiyani@tamu.edu>
2022-01-07 19:36:42 -0600

    Update Posts component to use updated Post model and component

--- src/components/feed/posts/Posts.tsx
+++ src/components/feed/posts/Posts.tsx
@@ -1,7 +1,33 @@
-const Posts = () => {
+import { useEffect, useState } from 'react';
+import jobPosts from '../../../data/Experience/JobData.json';
+import Post from './Post';
+import { JobPost, Post as PostData } from '../../../models/feed/posts/Post';
+
+const Posts = ({ category }: { category: string }) => {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(
+        () => {
+            switch (category.trim().toLowerCase()) {
+                case 'experience':
+                    const posts: PostData[] = jobPosts.map(
+                        post => new JobPost(post).toPost()
+                    );
+                    setPosts(posts);
+                    break;
+                default:
+                    break;
+            }
+        }, 
+        [category]
+    );
+
     return (
         <div>
-                        
+            {
+                posts.map(post => <Post post={post} />)
+            }
         </div>
     );
 }
