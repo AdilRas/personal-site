7f3d7f48083490106c4d56935e91590af22b42dc
Adil Rasiyani <adil.rasiyani@tamu.edu>
2022-01-07 19:37:26 -0600

    Add category state for Feed component

--- src/components/feed/Feed.tsx
+++ src/components/feed/Feed.tsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import Posts from "./posts/Posts";
 import Stories from "./stories/Stories";
 
 function Feed() {
+
+    const [category, setCategory] = useState("Experience");
+
     return (
         <main className="grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto">
             <section className="col-span-2">
                 {/* <Stories /> */}
-                <Posts />
+                <Posts category={category}/>
             </section>
 
             <section className="hidden xl:inline-grid md:col-span-1">
